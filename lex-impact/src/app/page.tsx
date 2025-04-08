ecbf35158f4b4a2e4992b08e741905789248537a
Elite-tch <elite6461@gmail.com>
2025-04-08 09:00:00 +0100

    newsletter section

--- lex-impact/src/app/page.tsx
+++ lex-impact/src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import Newsletter from "./components/Newsletter";
 export default function Home() {
   return (
     <div>
       {/* Mount the sections here  */}
+      <Newsletter/>
     </div>
   );
 }
